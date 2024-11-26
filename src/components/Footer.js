dc7bc0aaa5957510aca275d6fb4286f86f2c4cba
VanshMehta588 <161753906+VanshMehta588@users.noreply.github.com>
2024-11-26 16:12:34 +0530

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import logo from '../assets/Logo2.png'
 export default function Footer() {
     return (
         <>
-            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
                 <p className="col-md-5 mb-0 text-muted">© 2024 Shadow Clan Company, Inc</p>
-                <a href="/" className="col-md-3 align-items-center justify-content-center mb-0 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <a href="/" className="col-md-3 align-items-center justify-content-center mb-0 mb-md-0 me-md-auto link-dark text-decoration-none ">
                 <img className='text-center my-4' src={logo} width={200} height={100} alt="" />
                 </a>
 
