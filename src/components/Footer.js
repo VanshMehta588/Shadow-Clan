5d2d06153ce92b96c5c4a12384f77129bb4759d2
VanshMehta588 <161753906+VanshMehta588@users.noreply.github.com>
2024-11-26 16:06:47 +0530

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <>
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <p className="col-md-5 mb-0 text-muted">© 2024 Shadow Clan Company, Inc</p>
-                <a href="/" className="col-md-3 align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <a href="/" className="col-md-3 align-items-center justify-content-center mb-0 mb-md-0 me-md-auto link-dark text-decoration-none">
                 <img className='text-center my-4' src={logo} width={200} height={100} alt="" />
                 </a>
 
